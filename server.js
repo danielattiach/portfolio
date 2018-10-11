8e00f8ed23218db524c9b1f814efb8e2d3783605
Daniel Attiach <danielattiach@daniels-mbp.home>
2018-10-11 13:11:05 +0300

    deploy retry

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
+const path = require('path');
 
 require('./models/User');
 require('./services/passport');
@@ -44,18 +45,14 @@ mongoose.connect(db, {useNewUrlParser: true})
 // Use route files
 app.use('/auth', auth);
 
-app.get('/', (req, res) => {
-  res.send('Homepage new');
-})
-
+// Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
+  // Set static folder
   app.use(express.static('client/build'));
 
-  const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
-
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 const port = process.env.PORT || 5000;
