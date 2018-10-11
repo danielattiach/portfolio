a868e2992c3580acbd1e290c02d35b449b959abb
Daniel Attiach <danielattiach@Daniels-MacBook-Pro.local>
2018-10-11 17:20:06 +0300

    edited profile page

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux';
+import '../css/profile.css';
+
 class Profile extends Component {
   constructor(props) {
     super(props);
   }
   render() {
     return (
-      <div>
-        Name: <img src={this.props.user.avatar} width={24}/> {this.props.user.name}
+      <div className="container">
+        <div className="row">
+        <div className="col-sm text-center">
+            <img className="profile-avatar" src={this.props.user.avatar}/>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm text-center">
+            <h2 className="profile-title">{this.props.user.name}</h2>
+          </div>
+        </div>
       </div>
     )
   }
