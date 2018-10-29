56e3587548d6cef3c1f7cd6dcf5e90b2967f5722
Daniel Attiach <danielattiach@daniels-mbp.home>
2018-10-30 00:33:09 +0200

    changed favicon and removed placeholders from contact form

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -74,25 +74,25 @@ export default class Contact extends Component {
           <FormGroup row>
               <Label for="name" sm={2} size="sm">Name:</Label>
               <Col sm={10}>
-                <Input type="text" name="name" id="name" placeholder="name" onChange={this.handleNameChange}/>
+                <Input type="text" name="name" id="name" onChange={this.handleNameChange}/>
               </Col>
             </FormGroup>
             <FormGroup row>
               <Label for="email" sm={2} size="sm">Email:</Label>
               <Col sm={10}>
-                <Input type="email" name="email" id="email" placeholder="email" onChange={this.handleEmailChange}/>
+                <Input type="email" name="email" id="email" onChange={this.handleEmailChange}/>
               </Col>
             </FormGroup>
             <FormGroup row>
               <Label for="subject" sm={2} size="sm">Subject:</Label>
               <Col sm={10}>
-                <Input type="text" name="subject" id="subject" placeholder="subject" onChange={this.handleSubjectChange}/>
+                <Input type="text" name="subject" id="subject" onChange={this.handleSubjectChange}/>
               </Col>
             </FormGroup>
             <FormGroup row>
               <Label for="message" sm={2} size="sm">Message:</Label>
               <Col sm={10}>
-                <Input type="textarea" name="message" id="message" placeholder="message" onChange={this.handleMessageChange}/>
+                <Input type="textarea" name="message" id="message" onChange={this.handleMessageChange}/>
               </Col>
             </FormGroup>
             <div className="text-center"><Button onClick={this.onSubmit}>Submit</Button></div>
